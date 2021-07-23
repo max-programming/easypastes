6391d00672c2c9243099f23191e2d8c23a50f058
Max Programming <ui.sabuwala@gmail.com>
2021-07-23 21:05:14 +0530

    🚸 Added an alert for the user to add username

--- pages/user/pastes/[username].tsx
+++ pages/user/pastes/[username].tsx
@@ -6,6 +6,8 @@ import {
 	TabPanels,
 	Tab,
 	TabPanel,
+	Alert,
+	CloseButton,
 	Text
 } from '@chakra-ui/react';
 import {
@@ -21,6 +23,8 @@ import Paste from 'components/CodePastes/Paste';
 import supabaseClient from 'utils/supabase';
 import { SignedOut, WithUser } from '@clerk/clerk-react';
 import axios from 'axios';
+import useLocalStorage from 'use-local-storage';
+import Link from 'next/link';
 
 const links = [
 	{
@@ -32,6 +36,13 @@ const links = [
 		text: 'Pastes'
 	}
 ];
+
+interface Props {
+	pastes: PasteType[];
+	fullName: string;
+	id: string;
+	username: string;
+}
 
 export const getServerSideProps: GetServerSideProps = async context => {
 	const { data: users } = await axios.get<Array<User>>(
@@ -54,22 +65,36 @@ export const getServerSideProps: GetServerSideProps = async context => {
 		props: {
 			pastes,
 			fullName: `${currentUser.first_name} ${currentUser.last_name}`,
-			id: currentUser.id
+			id: currentUser.id,
+			username: currentUser.username
 		}
 	};
 };
 
-export default function MyPastes({
-	pastes,
-	fullName,
-	id
-}: {
-	pastes: PasteType[];
-	fullName: string;
-	id: string;
-}) {
+export default function MyPastes({ pastes, fullName, id, username }: Props) {
+	const [showAlert, setShowAlert] = useLocalStorage('username-alert', false);
 	return (
 		<Layout title={`${fullName} - Pastes`} links={links}>
+			<Alert
+				status="info"
+				variant="left-accent"
+				hidden={!showAlert || !!username}
+			>
+				Add username in&nbsp;
+				<Link href="/user" passHref>
+					<Text as="a" _hover={{ color: 'purple.200' }}>
+						the settings
+					</Text>
+				</Link>
+				&nbsp;for your custom profile URL
+				<CloseButton
+					onClick={() => setShowAlert(false)}
+					position="absolute"
+					right="8px"
+					top="8px"
+					title="Remove alert"
+				/>
+			</Alert>
 			<Container maxW="container.xl" mt="6">
 				<Heading textAlign="center" size="lg">
 					Pastes by {fullName}
