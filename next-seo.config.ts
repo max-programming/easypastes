f5a16b2086af73871b4512d577db8d768fefc4fb
Max Programming <ui.sabuwala@gmail.com>
2021-07-24 17:34:07 +0530

    🔍 Added favicon and image when sharing

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -6,19 +6,30 @@ const config = {
       content: 'paste, easypastes, create, pastebin, git, github, gitlab'
     }
   ],
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/logo.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/logo.png',
+      sizes: '100x100'
+    }
+  ],
   openGraph: {
     type: 'website',
     locale: 'en_US',
     url: 'https://easypastes.tk',
     description: `Use Easy Pastes to create, store, share code snippets by simply pasting them with syntax highlight.`,
-    // images: [
-    //   {
-    //     url: 'https://readme.so/readme.png',
-    //     width: 375,
-    //     height: 375,
-    //     alt: 'Og Image Alt'
-    //   }
-    // ],
+    images: [
+      {
+        url: 'https://easypastes.tk/logo.png',
+        width: 100,
+        height: 100,
+        alt: 'Og Image Alt'
+      }
+    ],
     site_name: 'Easy Pastes',
     imageWidth: 1200,
     imageHeight: 1200
