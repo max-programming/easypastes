48dc52109a6436632fb22512011cde989cf8dcf4
Max Programming <ui.sabuwala@gmail.com>
2021-07-27 14:23:37 +0530

    🚑 Filter zero width whitespace characters for swears

--- utils/filterBadWords.ts
+++ utils/filterBadWords.ts
@@ -1,7 +1,10 @@
 import Filter from 'bad-words';
 
 export default function filterBadWords(text: string): string {
-  if (text.trim() === '') return;
+  text = text
+    .split('')
+    .filter(c => c.charCodeAt(0) !== 8203)
+    .join('');
   const filter = new Filter();
   return filter.clean(text);
 }
