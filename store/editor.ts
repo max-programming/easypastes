5305ed847fd27fdfb2bdc303523a8e9220227f60
janaSunrise <warriordefenderz@gmail.com>
2022-05-25 23:56:48 +0530

    Add functions to the global editor store.

--- store/editor.ts
+++ store/editor.ts
@@ -3,18 +3,37 @@ import { persist } from 'zustand/middleware';
 
 import { ZustandSetType } from '../constants';
 
+type Indentation = 'spaces' | 'tabs';
+
 interface EditorState {
   ligatures: boolean;
-  indentation: 'spaces' | 'tabs';
+  indentation: Indentation;
   indentSize: number;
   wrapping: boolean;
+
+  toggleLigatures: () => void;
+  toggleWrapping: () => void;
+  setIndentation: (indentation: Indentation) => void;
+  setIndentSize: (indentSize: number) => void;
 }
 
 const editorStore = (set: ZustandSetType<EditorState>) => ({
   ligatures: true,
   indentation: 'spaces',
   indentSize: 4,
-  wrapping: true
+  wrapping: true,
+
+  toggleLigatures: () =>
+    set((state: EditorState) => ({ ...state, ligatures: !state.ligatures })),
+
+  toggleWrapping: () =>
+    set((state: EditorState) => ({ ...state, wrapping: !state.wrapping })),
+
+  setIndentation: (indentation: 'spaces' | 'tabs') =>
+    set((state: EditorState) => ({ ...state, indentation })),
+
+  setIndentSize: (indentSize: number) =>
+    set((state: EditorState) => ({ ...state, indentSize }))
 });
 
 export const useEditorStore = create(
