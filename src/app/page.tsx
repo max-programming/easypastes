bfbf4a13674c57195aa02dcf64e553d54c33f565
Usman S <ui.sabuwala@gmail.com>
2024-02-03 20:41:59 +0530

    Sign In and Sign Up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
 		<>
 			<div>Home page</div>
+			<div className="flex gap-2">
+				<Link href="/sign-in">Sign in</Link>
+				<Link href="/sign-up">Sign up</Link>
+			</div>
 		</>
 	);
 }
